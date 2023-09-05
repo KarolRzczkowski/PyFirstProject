fb23d4cce5c0d3ae69b85d8b496ec2c279f4b1bc
KarolRzczkowski <rzeczkowskikarol166@gmail.com>
2023-09-05 17:20:13 +0200

    first commit

--- passwordgen.py
+++ passwordgen.py
@@ -1,52 +1,8 @@
-import pyautogui
-import pymongo
 import random
-
-# Establish a connection to MongoDB
-client = pymongo.MongoClient('mongodb://localhost:27017')
-
-# Access the database
-db = client['mydatabase']
-
-# Access the collection
-collection = db['quiz']
-
-coords = (pyautogui.size().width - 1, 1)
-wronganswers = 0
-
-def QuizApp(correctanswer, wronganswer):
-    try:
-        while True:
-            # Pobierz wszystkie pytania z bazy danych
-            questions_data = list(collection.find({}))
-
-            if not questions_data:
-                break  
-           
-            question_data = random.choice(questions_data)
-            questions_data.remove(question_data)
-
-            question_text = question_data['question']
-            options = question_data['options']
-            
-            user_answer = input(f"{question_text} ({'/'.join(options.values())}): ")
-            
-            if user_answer.lower() == options[correctanswer].lower():
-                print('Correct Answer')
-            else:
-                print('Wrong Answer')
-                global wronganswers
-                wronganswers += 1
-                print(f"Wrong Answers Count: {wronganswers}")    
-                if wronganswers == 4:
-                    print('You are stupid')
-                    pyautogui.moveTo(*coords)
-                    pyautogui.countdown(2)
-                    pyautogui.click()
-                
-    except ValueError:
-        print('Value Error')
-    except Exception as e:
-        print(f'An error occurred: {e}')
-
-QuizApp('correct', 'wrong')
+import pyautogui
+#defining array
+arr = [1,2,3,4,5]
+randomindexofarr = random.randint(0, len(arr) - 1)
+if randomindexofarr == 2:
+ pyautogui.moveTo(-50 , 50)
+print(randomindexofarr)
